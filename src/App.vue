8df0387ef1ec1ea191f2cdd172ed43c2f441cbb6
Matthew Ess <mess@yelp.com>
2018-02-26 22:19:42 -0500

    ''

--- src/App.vue
+++ src/App.vue
@@ -28,6 +28,12 @@ export default {
 </script>
 
 <style lang="sass">
+@import '~bulma/sass/utilities/initial-variables'
+@import '~bulma/sass/utilities/functions'
+
+$primary: #FFDC46
+$primary-invert: findColorInvert($primary)
+
 @import '~bulma/bulma'
 @import '~bulma-divider/bulma-divider'
 </style>
